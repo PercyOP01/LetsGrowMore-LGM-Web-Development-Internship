0a62f98de3ba8a83ae621bb5068cbd39deeffee1
Abhishek Patil <72570748+PercyOP01@users.noreply.github.com>
2022-01-28 00:46:46 +0530

    Add files via upload

---
+++ LetsGrowMore-LGM-Web-Development-Internship-Task2/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return ( <>
+        <div className = 'navbar' >
+        <NavLink exact to = '/' className = 'logo' > GrowMore </NavLink> 
+        <NavLink exact to = 'usersdata' className = 'btn btn-primary getusers_btn' > Get Users </NavLink> 
+        </div> 
+        </>
+
+
+
+    )
+
+}
+export default Navbar;
